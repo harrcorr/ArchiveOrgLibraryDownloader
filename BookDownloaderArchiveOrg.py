7842730df5f7c3141c7dd289a659a649b47d5912
harrcorr <corriganharry@outlook.com>
2020-06-15 18:20:54 +0100

    Changed Many things
    added scale contol
    changed download method from selenium to requests
    added mulitithreading

--- BookDownloaderArchiveOrg.py
+++ BookDownloaderArchiveOrg.py
@@ -1,15 +1,19 @@
 from selenium import webdriver
 import time
 from selenium.webdriver.chrome.options import Options
-from PIL import Image
-from io import BytesIO
+import requests
 import os
+import threading
+import shutil
 chrome_options = Options()
 email = input("The email for your Archive.org account: ")
 password = input("And the password: ")
 url = input("Url of book you want to download: ")
 dir = input("Where do you want to save the books pages (relative to current directory) : ")
 pages = input("How many pages do you want to download: ")
+quality = input("What scale do you want the images 1-10 (lower is better res): ")
+print("Starting.. btw because of multi threading dont exit the program until it says PRESS ANY KEY TO CONTINUE that means all threads are completed")
+quality = int(quality)
 pages = int(pages)
 chrome_options.add_experimental_option("detach", True)
 chrome_options.add_argument("--headless")  
@@ -17,22 +21,21 @@ driver = webdriver.Chrome("./chromedriver.exe",chrome_options=chrome_options)
 driver.get("https://archive.org/account/login")
 user = driver.find_element_by_name("username")
 pwd = driver.find_element_by_name("password")
+cookies_dict = {}
 user.send_keys(email)
 pwd.send_keys(password+"\n")
 time.sleep(0.5)
-
+s = requests.Session()
 def Download(url,directory,bookname,pagenum):
     if(os.path.exists(directory)):
         pass
     else:
         os.mkdir(directory)
     print("Downloading Page: " + str(pagenum))
-    driver.get(url)
-    image = driver.find_element_by_tag_name("img").screenshot_as_png
-    im = Image.open(BytesIO(image))
-    name = directory + "/" + str(pagenum)+r".png"
-    im.save(name)
-
+    r = requests.get(url, stream=True,cookies=cookies_dict)
+    r.raw.decode_content = True
+    with open(directory+"/"+str(pagenum)+".jpg", 'wb') as out_file:
+        shutil.copyfileobj(r.raw, out_file)
 def split(string):
     splitstring = []
     for i in string:
@@ -44,17 +47,38 @@ def join(array):
     for i in array:
         result+=str(i)
     return result
-
+def FloatToString(float,digits):
+    float = split(str(float))[1:digits]
+    float = "".join(float)
+    float = float.replace(".","")
+    if(len(split(float)) != 4):
+        float = "0"+float
+    return float
 def GetPagesArray(srcurl,maxpages):
     pages = []
-    srcul = srcurl.split(".jp2")
-    for i in range(0,maxpages):
-        for c in range(0,len(str(i))):
-            srcul[0] = split(srcul[0])
-            srcul[0][(len(srcul[0])-1)-(c)]=split(str(i))[c]
-            new = join(srcul[0])
-        pages.append(new+".jp2"+srcul[1])
-        srcul=srcurl.split(".jp2")
+    srcurl = srcurl.split("&scale=4")
+    tmp = split(srcurl[0])
+    tmp[len(tmp)-1]="2&scale="+str(quality)
+    tmp = "".join(tmp)
+    srcurl = str("".join(tmp)+"".join(srcurl[1]))
+    modurl = srcurl
+    i=0.0001
+    while(len(split(str(maxpages))) != 4):
+        maxpages = "0"+str(maxpages)
+    if(len(split(maxpages)) == 4):
+        maxpages = "0."+maxpages
+    maxpages = float(maxpages)
+    while(i < maxpages):
+        modurl = modurl.split(".jp2")
+        spliturl = split(modurl[0])
+        spliturl = spliturl[:len(spliturl)-4]
+        value = split(FloatToString(i,6))
+        spliturl+=value
+        modurl = "".join(spliturl)+".jp2&"+modurl[1]
+        modurl = "".join(modurl)
+        pages.append(modurl)
+        modurl=srcurl
+        i+=0.0001
     return pages
 def GetBaseUrl(bookurl,num = 0):
     success = 0
@@ -65,13 +89,15 @@ def GetBaseUrl(bookurl,num = 0):
             base = driver.find_elements_by_class_name("BRpageimage")[1].get_attribute("src")
 
             success = 1
-        except:
+        except Exception as e:
             num=num+1
             pass
-    print(base)
+    cookies = driver.get_cookies()
+    for cookie in cookies:
+       cookies_dict[cookie['name']] = cookie['value']
     return base
 print("Finding pages....")
 download = GetPagesArray(GetBaseUrl(url),pages)
 print("Downloading pages....")
 for i in range(0,len(download)):
-    Download(download[i],dir,"",i)
+    threading.Thread(target=Download,args=(download[i],dir,"",i)).start()
